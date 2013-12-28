2ceae45c9e943edfd70a55f518b37edb2a622be4
micmarsh <michael.marsh42@gmail.com>
2013-12-28 12:58:34 -0500

    we want a remove function

--- Logic.elm
+++ Logic.elm
@@ -7,10 +7,13 @@ type MoveResult = (Bool, [Piece])
 moveIsLegal : MoveCheck -> Bool
 moveIsLegal (piece, pieces, position) = True
 
+remove : [a] -> a -> [a]
+remove list element = filter (\e -> not <| e == element) list
+
 -- really need let destructures
 move : MoveCheck -> MoveResult
 move ((Piece t oldPos c), pieces, position) =
-    let removeOld = filter (\p -> not <| p == (Piece t oldPos c)) pieces
+    let removeOld = remove pieces (Piece t oldPos c)
         newPiece = Piece t position c
     in (True, newPiece :: removeOld)
 
