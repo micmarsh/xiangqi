99dfa188958471a3e0a9510cde1f9fcaefbdd90d
micmarsh <michael.marsh42@gmail.com>
2013-12-28 11:52:13 -0500

    can select any piece nbd

--- GameState.elm
+++ GameState.elm
@@ -1,19 +1,22 @@
 module GameState where
-import Model (Color, Red, Black)
+import Model (Color, Red, Black, Position, Piece, allPieces, findPiece)
 import Input
 import Mouse
 
-type State = {turn : Color}
-initialState = {turn = Red}
+type State = {turn : Color, selected: Maybe Piece, pieces: [Piece]}
+initialState = {turn = Red, selected = Nothing, pieces = allPieces}
 
-whoseTurn state redPos blackPos =  case state.turn of
-    Red -> redPos
-    Black -> blackPos
+type Positions = {red: Position, black: Position}
+posRecord red black = {red = red, black = black}
+unifiedPosition = lift2 posRecord Input.redBoardPosition Input.blackBoardPosition
 
-unifiedPosition = lift3 whoseTurn gameState Input.redBoardPosition Input.blackBoardPosition
+currentPos positions turn = case turn of
+    Red -> positions.red
+    Black -> positions.black
 
-update position state = case state.turn of
-    Red -> {turn = Black}
-    Black -> {turn = Red}
+update positions {turn, selected, pieces} =
+    let position = currentPos positions turn
+        piece = findPiece pieces position
+    in {turn = turn, selected = piece, pieces = pieces}
 
 gameState = foldp update initialState (sampleOn Mouse.clicks unifiedPosition)
