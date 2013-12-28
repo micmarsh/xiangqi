d00ffdcb99a3b7f16060eab026450f3a3640069c
micmarsh <michael.marsh42@gmail.com>
2013-12-27 19:37:09 -0500

    need Input.elm

---
+++ Input.elm
@@ -0,0 +1,29 @@
+module Input (boardPosition) where
+import Constants (boardWidth, boardHeight, squareSize, num2Char)
+import BoardState (Black, Red)
+import Mouse
+import Window
+
+toBoardPixels (x, y) w h = let yToBoard = (h - boardHeight) `div` 2
+                               xToBoard = (w - boardWidth) `div` 2
+                           in (x - xToBoard, y - yToBoard)
+boardPixels = lift3 toBoardPixels Mouse.position Window.width Window.height
+
+heightOffset = 12
+widthOffset = 20
+toBoardRow player pixels = case player of
+                            Red -> let newHeight = boardHeight + heightOffset
+                                       converted = newHeight - pixels
+                                   in (div converted squareSize) + 1
+                            Black -> (div pixels squareSize) + 1
+                        -- TODO these both need to be shifted a little bit in their respective directions,
+                        -- for more accuracy as to what you're actually pressing
+toBoardColumn player pixels = case player of
+                            Red -> num2Char <| div (pixels + widthOffset) squareSize
+                            Black -> let newWidth = boardWidth + widthOffset
+                                         converted = newWidth - pixels
+                                     in num2Char <| div converted squareSize
+
+toBoardPosition (x, y) = (toBoardColumn Red x, toBoardRow Red y)
+
+boardPosition = lift toBoardPosition boardPixels
