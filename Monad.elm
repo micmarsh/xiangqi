5f1bdea922cce5138ba19dca713b0060df41f29b
micmarsh <michael.marsh42@gmail.com>
2014-01-03 10:45:52 -0500

    types for monad functions

--- Monad.elm
+++ Monad.elm
@@ -1,20 +1,19 @@
 module Monad where
 
-type M = Maybe a
 
-map : (a -> b) -> M -> Maybe b
+map : (a -> b) -> Maybe a -> Maybe b
 map fn monad =
     case monad of
         Nothing -> Nothing
         Just thing -> fn thing |> Just
 
-filter : (a -> Bool) -> M -> M
+filter : (a -> Bool) -> Maybe a -> Maybe a
 filter fn monad =
     case monad of
         Nothing -> Nothing
         Just thing -> if fn thing then monad else Nothing
 
-flatmap : (a -> Maybe b) -> M -> Maybe b
+flatmap : (a -> Maybe b) -> Maybe a -> Maybe b
 flatmap fn monad =
     case monad of
         Nothing -> Nothing
