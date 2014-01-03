a819d508b23d2e6afae80439f62ebe29a363b9ef
micmarsh <michael.marsh42@gmail.com>
2014-01-03 10:46:51 -0500

    actual M type

--- Monad.elm
+++ Monad.elm
@@ -8,13 +8,13 @@ map fn monad =
         Nothing -> Nothing
         Just thing -> fn thing |> Just
 
-filter : (a -> Bool) -> Maybe a -> Maybe a
+filter : (a -> Bool) -> M a -> M a
 filter fn monad =
     case monad of
         Nothing -> Nothing
         Just thing -> if fn thing then monad else Nothing
 
-flatmap : (a -> Maybe b) -> Maybe a -> Maybe b
+flatmap : (a -> M b) -> M a -> M b
 flatmap fn monad =
     case monad of
         Nothing -> Nothing
