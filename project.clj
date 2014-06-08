2fe672bc86d7e6591099265f9b8c53aefc5b99ab
micmarsh <mike@marsh.pw>
2014-06-08 12:59:12 -0400

    $1

--- project.clj
+++ project.clj
@@ -1,6 +1,17 @@
-(defproject xiangi "0.1.0-SNAPSHOT"
+(defproject xiangqi "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojurescript "0.0-2069"]
+                 [org.clojure/clojure "1.5.1"]
+                 [reagent "0.4.2"]]
+
+  :plugins [[lein-cljsbuild "1.0.0"]]
+
+  :cljsbuild
+      {:builds [{:source-paths ["src/xiangqi"]
+                   :compiler
+                     {:preamble ["reagent/react.min.js"]
+                      :output-to "resources/main.js"
+                      :pretty-print true}}]}))
