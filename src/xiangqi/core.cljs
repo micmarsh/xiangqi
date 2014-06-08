f7280b37f469e5c1276702550669da3f9c6d22c2
micmarsh <mike@marsh.pw>
2014-06-08 15:43:53 -0400

    some images!

--- src/xiangqi/core.cljs
+++ src/xiangqi/core.cljs
@@ -1,5 +1,8 @@
 (ns xiangqi.core
-    (:require [reagent.core :as r]))
+    (:require [reagent.core :as r]
+              [xiangqi.game :refer [game->board]]))
+
+(def game (js/Game.))
 
 (set! *print-fn* #(.log js/console %))
 
@@ -11,23 +14,25 @@
 (defn main-view [pieces]
     [:table 
         (for [[row index] (enumerate @pieces)]
-           ^{:key index} ; later: the actual index, row-col type  
+           ^{:key index}
             [:tr
                 (for [[square column] (enumerate row)]
-                    ^{:key [index column]} ; later: the actual row-col position, that won't ever change
-                    [:td 
-                        [:img {:src "assets/red/cannon.png"}]])])])
+                    ^{:key [index column]} 
+                    ; TODO "square" means piece right now. Also, player color
+                    [:td [:img
+                        {:src (str "assets/" (:color square) "/" 
+                                (:name square) ".png")}]])])])
 
-(def pieces (r/atom (vec (for [row (range 8)] 
-                        (vec (for [column (range 8)] 
-                            (str row "," column)))))))
+(def pieces (-> game game->board atom))
 
 (r/render-component
     [main-view pieces] 
     (.getElementById js/document "main"))
 
-(aset js/window "setText" (fn [row column text]
-    (swap! pieces (fn [pieces]
-                    (let [row-vec (pieces row)]
-                        (assoc pieces row 
-                            (assoc row-vec column text)))))))
+(aset js/window "setText" 
+    (fn [row column text]
+        (swap! pieces 
+            (fn [pieces]
+                (let [row-vec (pieces row)]
+                    (assoc pieces row 
+                        (assoc row-vec column text)))))))
