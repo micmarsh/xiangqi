ce5daeaca926e3de88c84203e7428548833e45c7
micmarsh <mike@marsh.pw>
2014-06-08 17:58:35 -0400

    getting color in place

--- src/xiangqi/info.cljs
+++ src/xiangqi/info.cljs
@@ -15,14 +15,34 @@
 (def game-info (atom { }))
 
 (def slice-from #(.slice %2 %1))
-(defn register-hash! [window]
+(defn register-hash! [window game-info]
     (->> js/window
         get-hash 
         (slice-from 1) 
-        (swap! game-info assoc :id)))
+        (swap! game-info assoc :game-id)))
+
+(defn color-key [id]
+    (str game-id "-player-color"))
+
+(defn get-color [window]
+    (-> window 
+        .-localStorage 
+        (aget (color-key game-id))))
+
+(defn has-color? [window game-id]
+    (-> window get-color js/Boolean)
+
+(defn set-color! [window id player-color]
+    (aset (.-localStorage window)
+        (color-key id)
+        player-color))
+
+(defn register-color! [window]
+    (let [color (get-color window)]
+        (swap! game-info assoc :player-color color)))
 
 (if (has-hash? js/window)
-    (register-hash! js/window)
+    (register-hash! js/window game-info)
     (do 
         (set-hash! js/window (new-id))
-        (register-hash! js/window)))
+        (register-hash! js/window) game-info))
